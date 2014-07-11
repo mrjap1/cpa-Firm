defb901e19b8dbbea2a831e134417c84066d10e0
James Arthur Phillips I <mrjap1jobs@gmail.com>
2014-07-11 12:57:44 -0400

    Files for my CPA Firm
    Certified Public Accountants Firm

---
+++ js/html5script4ie.js
@@ -0,0 +1,22 @@
+// JScript source code
+
+
+// JavaScript Document
+
+ for(var e,l='article aside figcaption figure footer header hgroup nav section time'.split(' ');e=l.pop();document.createElement(e))
+
+ 
+// Javascript for HTML5 
+	document.createElement('caption');
+	document.createElement('footer');
+	document.createElement('header');
+	document.createElement('section');
+	document.createElement('aside');
+	document.createElement('hgroup');
+	document.createElement('video');
+	document.createElement('nav');
+	
+	
+
+
+
